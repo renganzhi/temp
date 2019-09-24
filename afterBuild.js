5b8e4f0ce17e6c674c2c8341f02c73ed987a3173
zhaoyisu <zhaoys@uxsino.com>
2019-09-24 16:05:39 +0800

    添加打包后自动删除重复的文件引入

--- afterBuild.js
+++ afterBuild.js
@@ -9,6 +9,11 @@ var data = ''
 var arguments = process.argv.splice(2)
 var replaceText = arguments[0] || '/leaderview-static/' // 目标目录
 
+// var ignoreJs = ['jquery', 'jquery-ui']
+var ignoreJs1 = ['jquery', 'jquery-ui']
+var ignoreJs = ['jquery', 'jquery-ui', 'select2', 'bootstrap-table']
+var ignoreCss = ['select2', 'css\/bootstrap', 'bootstrap-tab']
+
 var readStream = fs.createReadStream(sourceFile)
 var writeStream = fs.createWriteStream(destPath)
 readStream.setEncoding('UTF8')
@@ -17,12 +22,32 @@ readStream.on('data', function (chunk) {
   var reg = /\.\/static\//g
   data = data.replace(/\r\n/g, '')
   data = data.replace(reg, replaceText)
+  data = data.replace(/<title>.+?<\/title>/, '')
+  data = data.replace(/<\!DOCTYPE html>/, '')
+  data = data.replace(/<html>/, '')
+  data = data.replace(/<\/html>/, '')
+  data = data.replace(/<body>/, '')
+  data = data.replace(/<\/body>/, '')
+
+  ignoreJs.forEach(function (item) {
+    var jsReg = new RegExp('(<script){1} (src=https){1}(.+?)' + item + '(.+?)(script>){1}', 'm')
+    data = data.replace(jsReg, '')
+  })
+  ignoreJs1.forEach(function (item) {
+    var jsReg = new RegExp('(<script.+?' + item + '.+?<\/script>)', 'm')
+    data = data.replace(jsReg, '')
+  })
+  ignoreCss.forEach(function (item) {
+    // var cssReg = new RegExp('<link.+?' + item + '.+?.css>', 'im')
+    var cssReg = new RegExp('<link .+?' + item + '.+?(.css){1}>', 'im')
+    data = data.replace(cssReg, '')
+  })
   writeStream.write(data, 'UTF8')
   writeStream.end()
 })
 // readStream.pipe(writeStream); 仅流式复制，不修改
 writeStream.on('finish', function () {
-  console.log('\x1B[32m main.html生成成功 \x1B[0m')
+  console.log('\x1B[32m leaderview：main.html生成成功 \x1B[0m')
 })
 writeStream.on('error', function (err) {
   console.log(err.stack)
--- build/build.js
+++ build/build.js
@@ -1,8 +1,8 @@
 'use strict'
 require('./check-versions')()
-
 process.env.NODE_ENV = 'production'
 
+const fs = require('fs')
 const ora = require('ora')
 const rm = require('rimraf')
 const path = require('path')
@@ -31,6 +31,59 @@ rm(path.join(config.build.assetsRoot, config.build.assetsSubDirectory), err => {
       console.log(chalk.red('  Build failed with errors.\n'))
       process.exit(1)
     }
+
+    // 打包之后自动生成simo项目中的大屏入口main.html文件
+    var fileName = 'index.html'
+    var targetName = 'main.html'
+    // path.resolve(__dirname, '..')
+    var sourceFile = path.resolve(__dirname, '../dist', fileName)
+    var destPath = path.resolve(__dirname, '../dist', targetName)
+    var data = ''
+    // var arguments = process.argv.splice(2)
+    var replaceText = '/leaderview-static/' // 目标目录
+
+    var ignoreJs1 = ['jquery', 'jquery-ui']
+    var ignoreJs = ['jquery', 'jquery-ui', 'select2', 'bootstrap-table']
+    var ignoreCss = ['select2', 'css\/bootstrap', 'bootstrap-tab']
+
+    var readStream = fs.createReadStream(sourceFile)
+    var writeStream = fs.createWriteStream(destPath)
+    readStream.setEncoding('UTF8')
+    readStream.on('data', function (chunk) {
+      data += chunk
+      var reg = /\.\/static\//g
+      data = data.replace(/\r\n/g, '')
+      data = data.replace(reg, replaceText)
+      data = data.replace(/<title>.+?<\/title>/, '')
+      data = data.replace(/<\!DOCTYPE html>/, '')
+      data = data.replace(/<html>/, '')
+      data = data.replace(/<\/html>/, '')
+      data = data.replace(/<body>/, '')
+      data = data.replace(/<\/body>/, '')
+
+      ignoreJs.forEach(function (item) {
+        var jsReg = new RegExp('(<script){1} (src=https){1}(.+?)' + item + '(.+?)(script>){1}', 'm')
+        data = data.replace(jsReg, '')
+      })
+      ignoreJs1.forEach(function (item) {
+        var jsReg = new RegExp('(<script.+?' + item + '.+?<\/script>)', 'm')
+        data = data.replace(jsReg, '')
+      })
+      ignoreCss.forEach(function (item) {
+        // var cssReg = new RegExp('<link.+?' + item + '.+?.css>', 'im')
+        var cssReg = new RegExp('<link .+?' + item + '.+?(.css){1}>', 'im')
+        data = data.replace(cssReg, '')
+      })
+      writeStream.write(data, 'UTF8')
+      writeStream.end()
+    })
+    // readStream.pipe(writeStream); 仅流式复制，不修改
+    writeStream.on('finish', function () {
+      console.log('\x1B[32m leaderview：main.html生成成功 \x1B[0m')
+    })
+    writeStream.on('error', function (err) {
+      console.log(err.stack)
+    })
 
     console.log(chalk.cyan('  Build complete.\n'))
     console.log(chalk.yellow(
