8aa5d8dcad91c5fca5e0dae9f377468976d1b84d
zhaoyisu <zhaoys@uxsino.com>
2019-09-28 13:50:48 +0800

    缺陷 #58653 时间器修改字体变大超出控件没有自动调整控件大小；更新图标文件解决相关缺陷

--- simo-leaderview-vue/src/components/Common/EditComp/Vtime.vue
+++ simo-leaderview-vue/src/components/Common/EditComp/Vtime.vue
@@ -12,12 +12,27 @@ export default {
     return {
       showTime: '',
       refreshTime: 1, // 设置为1误差最小
-      timeoutId: 0
+      timeoutId: 0,
+      sizeObj: {
+        'f12': { w: 44, h: 17 },
+        'f13': { w: 48, h: 18 },
+        'f14': { w: 51, h: 19 },
+        'f16': { w: 60, h: 22 },
+        'f18': { w: 66, h: 25 },
+        'f20': { w: 73, h: 28 },
+        'f28': { w: 101, h: 39 },
+        'f36': { w: 131, h: 51 },
+        'f48': { w: 175, h: 68 },
+        'f72': { w: 262, h: 102 }
+      }
     }
   },
   computed: {
     timeStyle: function () {
       return {
+        width: this.item.width + 'px',
+        height: this.item.height + 'px',
+        overflow: 'hidden',
         color: this.item.clr + ' !important',
         fontSize: this.item.fontSize + 'px !important'
         // border: this.item.bdpx + 'px solid ' + this.item.bdClr + ' !important',
@@ -55,8 +70,25 @@ export default {
     }
   },
   watch: {
-    'item.timeType': function (newV) {
+    'item.timeType': function (newV, oldV) {
       this.initTime(newV)
+      if (newV > oldV) {
+        let len = this.showTime.length
+        let width = parseInt(this.sizeObj['f' + this.item.fontSize].w / 8 * len)
+        if (this.item.width < width) {
+          this.item.width = width + 20
+        }
+      }
+    },
+    'item.fontSize': function (newV) {
+      let len = this.showTime.length
+      let width = parseInt(this.sizeObj['f' + newV].w / 8 * len)
+      if (this.item.width < width) {
+        this.item.width = width + 20
+      }
+      if (this.item.height < this.sizeObj['f' + newV].h) {
+        this.item.height = this.sizeObj['f' + newV].h + 20
+      }
     }
   },
   beforeDestroy () {
