82e5c08ae2b056405948cb951be5ded502ca741f
xu_zhudong <xu_zhudong@uxsino.com>
2019-11-07 11:48:26 +0800

    [缺陷修复] 解决缺陷 #61685 超级管理员大屏可见全部用户的页面

--- simo-leaderview/src/main/java/com/uxsino/leaderview/rpc/MCService.java
+++ simo-leaderview/src/main/java/com/uxsino/leaderview/rpc/MCService.java
@@ -6,14 +6,18 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 /**
  * @description 调用SIMO-MC的服务类
- * 已经可以正常调用MC中的方法了，但暂时没有需要使用的地方。
+ * 在调用MC服务类的时候，输入流被关闭，无法正常调用，测试环境上无法正常运行
  * @date 2019年10月29日
  */
 @FeignClient(value = "${service-mc}", fallback = MCClientHystrixLeaderview.class)
 public interface MCService {
     @RequestMapping(method = RequestMethod.GET, value = "mc/getMenu", consumes = "application/json")
     JsonModel getMenu(@RequestHeader("Cookie") String cookie);
+
+    @RequestMapping(method = RequestMethod.GET, value = "mc/role/findAllUserByRoleId", consumes = "application/json")
+    JsonModel findAllUserByRoleId(@RequestHeader("Cookie") String cookie, @RequestParam("roleIds") Long[] roleIds);
 }
