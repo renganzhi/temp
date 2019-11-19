37b8fc57f7518c3abb3ae556986661d882fdb6fa
zhaoyisu <zhaoys@uxsino.com>
2019-11-19 16:23:10 +0800

    fix: "配置资源指标详情“弹窗样式修改，目标占比图图例可见性修改

--- simo-leaderview-vue/src/components/Edit/Edit.vue
+++ simo-leaderview-vue/src/components/Edit/Edit.vue
@@ -823,10 +823,10 @@
           </div>
           <div class="modal-body" style="height: 450px; overflow: auto;">
             <form autocomplete="off" style="margin-bottom: 20px;" v-for="(list, i) in syst.windowObj" :key="i">
-              <div class="form-group modal-label" style="width: 100%; height: auto;">
+              <div class="form-group modal-label" style="width: 100%; min-height: 30px; height: auto;">
                 <label class="page-lable page-title"><i class="icon-n-arrowRight"></i>指标： {{list.indicator.name}}</label>
                 <div class="page-lable-content" style="margin-left: 32px;" v-if="list.fields && list.fields.length > 0">
-                  <span>属性：</span>
+                  <span style="float: left; margin-top: 5px;">属性：</span>
                   <Select2 v-model="syst.windowData[i].fields" :mapSelect="true" :multip="true" :maxLength="5" :obj="list.fields"></Select2>
                 </div>
               </div>
