22bda87ea4c6696975611f816daf90173cfc8edd
xu_zhudong <xu_zhudong@uxsino.com>
2019-12-03 11:47:35 +0800

    [功能] #57826 用户管理工作移交广播处理-大屏展示-页面配置

---
+++ simo-leaderview/src/main/java/com/uxsino/leaderview/handler/LeaderViewAuthorityHandler.java
@@ -0,0 +1,18 @@
+package com.uxsino.leaderview.handler;
+
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
+import org.springframework.stereotype.Component;
+
+@Component
+public class LeaderViewAuthorityHandler {
+
+    public void handle(JSONArray arr){
+
+    }
+
+    public void handleDomainChange(JSONObject obj){
+
+    }
+
+}
