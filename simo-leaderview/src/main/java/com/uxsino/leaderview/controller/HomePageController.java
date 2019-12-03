1b3f3d04cf53286aa1b78f447b905b91ac515c6f
xu_zhudong <xu_zhudong@uxsino.com>
2019-12-03 17:11:31 +0800

    [修复] 解决大屏展示中视频数据条无法拖动的问题

--- simo-leaderview/src/main/java/com/uxsino/leaderview/controller/HomePageController.java
+++ simo-leaderview/src/main/java/com/uxsino/leaderview/controller/HomePageController.java
@@ -5,6 +5,7 @@ import java.text.SimpleDateFormat;
 import java.util.*;
 import java.util.stream.Collectors;
 import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import javax.transaction.Transactional;
@@ -410,37 +411,111 @@ public class HomePageController {
 
     @ApiOperation("根据视频文件名获取视频文件")
     @RequestMapping(value = "/file/getVideo/{fileName}", method = RequestMethod.GET)
-    public void getVideo(@ApiParam("视频名") @PathVariable String fileName,
+    public void getVideo(@ApiParam("视频名") @PathVariable String fileName, HttpServletRequest request,
                               HttpServletResponse resp, HttpSession session){
         VideoFile videoFileInfo = videoFileService.getByName(fileName);
+        BufferedInputStream bis = null;
         if (ObjectUtils.isEmpty(videoFileInfo)){
             logger.error("文件 -> {} not exists ！", fileName);
             return;
         }
-        InputStream ins = null;
-        OutputStream ops = null;
         try {
-            resp.setContentType(videoFileInfo.getExtension());
             File file = new File(videoFileInfo.getFilePath());
-            ins = new FileInputStream(file);
-            ops = new BufferedOutputStream(resp.getOutputStream());
-            byte[] buff = new byte[1024];
-            int n;
-            while ((n=ins.read(buff))!= -1){
-                ops.write(buff,0,n);
+            if (file.exists()) {
+                long p = 0L;
+                long toLength = 0L;
+                long contentLength = 0L;
+                int rangeSwitch = 0; // 0,从头开始的全文下载；1,从某字节开始的下载（bytes=27000-）；2,从某字节开始到某字节结束的下载（bytes=27000-39000）
+                long fileLength;
+                String rangBytes = "";
+                fileLength = file.length();
+
+                // get file content
+                InputStream ins = new FileInputStream(file);
+                bis = new BufferedInputStream(ins);
+
+                // tell the client to allow accept-ranges
+                resp.reset();
+                resp.setHeader("Accept-Ranges", "bytes");
+
+                // client requests a file block download start byte
+                String range = request.getHeader("Range");
+                if (range != null && range.trim().length() > 0 && !"null".equals(range)) {
+                    resp.setStatus(javax.servlet.http.HttpServletResponse.SC_PARTIAL_CONTENT);
+                    rangBytes = range.replaceAll("bytes=", "");
+                    if (rangBytes.endsWith("-")) { // bytes=270000-
+                        rangeSwitch = 1;
+                        p = Long.parseLong(rangBytes.substring(0, rangBytes.indexOf("-")));
+                        contentLength = fileLength - p; // 客户端请求的是270000之后的字节（包括bytes下标索引为270000的字节）
+                    } else { // bytes=270000-320000
+                        rangeSwitch = 2;
+                        String temp1 = rangBytes.substring(0, rangBytes.indexOf("-"));
+                        String temp2 = rangBytes.substring(rangBytes.indexOf("-") + 1, rangBytes.length());
+                        p = Long.parseLong(temp1);
+                        toLength = Long.parseLong(temp2);
+                        contentLength = toLength - p + 1; // 客户端请求的是 270000-320000 之间的字节
+                    }
+                } else {
+                    contentLength = fileLength;
+                }
+
+                // 如果设设置了Content-Length，则客户端会自动进行多线程下载。如果不希望支持多线程，则不要设置这个参数。
+                // Content-Length: [文件的总大小] - [客户端请求的下载的文件块的开始字节]
+                resp.setHeader("Content-Length", new Long(contentLength).toString());
+
+                // 断点开始
+                // 响应的格式是:
+                // Content-Range: bytes [文件块的开始字节]-[文件的总大小 - 1]/[文件的总大小]
+                if (rangeSwitch == 1) {
+                    String contentRange = new StringBuffer("bytes ").append(new Long(p).toString()).append("-")
+                            .append(new Long(fileLength - 1).toString()).append("/")
+                            .append(new Long(fileLength).toString()).toString();
+                    resp.setHeader("Content-Range", contentRange);
+                    bis.skip(p);
+                } else if (rangeSwitch == 2) {
+                    String contentRange = range.replace("=", " ") + "/" + new Long(fileLength).toString();
+                    resp.setHeader("Content-Range", contentRange);
+                    bis.skip(p);
+                } else {
+                    String contentRange = new StringBuffer("bytes ").append("0-").append(fileLength - 1).append("/")
+                            .append(fileLength).toString();
+                    resp.setHeader("Content-Range", contentRange);
+                }
+
+                resp.setContentType("application/octet-stream");
+                resp.addHeader("Content-Disposition", "attachment;filename=" + fileName);
+
+                OutputStream out = resp.getOutputStream();
+                int n = 0;
+                long readLength = 0;
+                int bsize = 1024;
+                byte[] bytes = new byte[bsize];
+                if (rangeSwitch == 2) {
+                    // 针对 bytes=27000-39000 的请求，从27000开始写数据
+                    while (readLength <= contentLength - bsize) {
+                        n = bis.read(bytes);
+                        readLength += n;
+                        out.write(bytes, 0, n);
+                    }
+                    if (readLength <= contentLength) {
+                        n = bis.read(bytes, 0, (int) (contentLength - readLength));
+                        out.write(bytes, 0, n);
+                    }
+                } else {
+                    while ((n = bis.read(bytes)) != -1) {
+                        out.write(bytes, 0, n);
+                    }
+                }
+                out.flush();
+                out.close();
+                bis.close();
             }
-            ops.flush();
-        } catch (IOException e) {
-            logger.error("文件读取失败", e);
+        } catch (IOException ie) {
+            logger.error("文件读取失败", ie);
+            ie.printStackTrace();
+        } catch (Exception e) {
+            logger.error("流关闭异常", e);
             e.printStackTrace();
-        } finally {
-            try {
-                ins.close();
-                ops.close();
-            } catch (IOException e) {
-                logger.error("流关闭异常", e);
-                e.printStackTrace();
-            }
         }
     }
 
