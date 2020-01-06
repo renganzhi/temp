6120e401327e38bd2a797a6e0837493439508824
zhaoyisu <zhaoys@uxsino.com>
2020-01-06 11:33:23 +0800

    update

--- simo-leaderview-vue/src/App.vue
+++ simo-leaderview-vue/src/App.vue
@@ -30,7 +30,7 @@ export default {
     var _hash = window.location.hash
     if (_hash.split('/')[1]) {
       var _url = window.location.protocol + '//' + window.location.host + '/index'
-      // window.location.href = _url
+      window.location.href = _url
     }
   }
 }
