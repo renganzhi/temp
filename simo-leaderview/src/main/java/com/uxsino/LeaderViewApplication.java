dd1bbc368cb35133f50a4c69845c6c8cbb81f323
nixj <nixj@uxsino.com>
2020-06-08 14:45:33 +0800

    其他：调整日志格式和编译脚本

--- simo-leaderview/src/main/java/com/uxsino/LeaderViewApplication.java
+++ simo-leaderview/src/main/java/com/uxsino/LeaderViewApplication.java
@@ -1,5 +1,7 @@
 package com.uxsino;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
@@ -22,9 +24,10 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableOAuth2Sso
 @EnableSwagger2
 public class LeaderViewApplication {
-
     public static void main(String[] args) {
+        Logger logger = LoggerFactory.getLogger(LeaderViewApplication.class);
         SpringApplication.run(LeaderViewApplication.class, args);
+        logger.info("LeaderView启动完成");
     }
 
 }
