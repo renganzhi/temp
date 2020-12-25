e7634650db6591a8322e8b7619a82c495e954d9a
Zhao Chen <zhaochen@uxsino.com>
2020-12-25 15:34:40 +0800

    [feature] 97538 边框组件内容新增

--- simo-leaderview-vue/src/config/settings.js
+++ simo-leaderview-vue/src/config/settings.js
@@ -1,13 +1,75 @@
 var env = process.env.NODE_ENV
-var settings = {
-  // 这里配置一些全局变量
-  gbs: {
-    host: env === 'development' ? '/api' : window.location.protocol + '//' + window.location.host,
-    inDev: env === 'development'
-    // host: 'http://' + window.location.host // 打包的前缀
-    // host: 'http://localhost:9999/leaderview'
+
+
+export const gbs = {
+  host: env === 'development' ? '/api' : window.location.protocol + '//' + window.location.host,
+  inDev: env === 'development'
+  // host: 'http://' + window.location.host // 打包的前缀
+  // host: 'http://localhost:9999/leaderview'
+};
+
+let titleCase = [
+  {
+    mini: '/leaderview/border/titleMini1.png',
+    imgSrc: '/leaderview/border/titleBg1.png'
   },
-  baseData: {
+  {
+    mini: '/leaderview/border/titleMini2.png',
+    imgSrc: '/leaderview/border/titleBg2.png'
+  },
+  {
+    mini: '/leaderview/border/titleBg3.png',
+    imgSrc: '/leaderview/border/titleBg3.png'
+  },
+  {
+    mini: '/leaderview/border/titleBg4.png',
+    imgSrc: '/leaderview/border/titleBg4.png'
+  },
+  {
+    mini: '/leaderview/border/titleBg5.png',
+    imgSrc: '/leaderview/border/titleBg5.png'
+  }
+];
+
+for (let i in new Array(8).fill(0)){
+  // console.log('i: ', typeof i);
+  const src = `/leaderview/border/titleBg${6+Number(i)}.svg`
+  titleCase.push({
+    mini: src,
+    imgSrc: src
+  })
+}
+
+let cardCase = [
+  {
+    mini: '/leaderview/border/cardMini1.png',
+    imgSrc: '/leaderview/border/cardBg1.png'
+  },
+  {
+    mini: '/leaderview/border/cardMini2.png',
+    imgSrc: '/leaderview/border/cardBg2.png'
+  },
+  {
+    mini: '/leaderview/border/cardMini3.png',
+    imgSrc: '/leaderview/border/cardBg3.png'
+  },
+  {
+    mini: '/leaderview/border/cardBg4.png',
+    imgSrc: '/leaderview/border/cardBg4.png'
+  }
+];
+
+for (let i in new Array(11).fill(0)){
+  // console.log('i: ', typeof i);
+  const src = `/leaderview/border/cardBg${5+Number(i)}.svg`
+  cardCase.push({
+    mini: src,
+    imgSrc: src
+  })
+}
+
+
+export const baseData = {
     allowOverflow: 20, // 允许超出画布的范围
     home: {
       // 主页大屏的默认配置
@@ -41,46 +103,6 @@ var settings = {
         fontName: '0123456789'
       }
     ],
-    cardCase: [
-      {
-        mini: '/leaderview/border/cardMini1.png',
-        imgSrc: '/leaderview/border/cardBg1.png'
-      },
-      {
-        mini: '/leaderview/border/cardMini2.png',
-        imgSrc: '/leaderview/border/cardBg2.png'
-      },
-      {
-        mini: '/leaderview/border/cardMini3.png',
-        imgSrc: '/leaderview/border/cardBg3.png'
-      },
-      {
-        mini: '/leaderview/border/cardBg4.png',
-        imgSrc: '/leaderview/border/cardBg4.png'
-      }
-    ],
-    titleCase: [
-      {
-        mini: '/leaderview/border/titleMini1.png',
-        imgSrc: '/leaderview/border/titleBg1.png'
-      },
-      {
-        mini: '/leaderview/border/titleMini2.png',
-        imgSrc: '/leaderview/border/titleBg2.png'
-      },
-      {
-        mini: '/leaderview/border/titleBg3.png',
-        imgSrc: '/leaderview/border/titleBg3.png'
-      },
-      {
-        mini: '/leaderview/border/titleBg4.png',
-        imgSrc: '/leaderview/border/titleBg4.png'
-      },
-      {
-        mini: '/leaderview/border/titleBg5.png',
-        imgSrc: '/leaderview/border/titleBg5.png'
-      }
-    ]
+    cardCase,
+    titleCase,
   }
-}
-module.exports = settings
