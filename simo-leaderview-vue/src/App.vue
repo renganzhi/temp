a78b121f4e5f26b03530f5cd48f14502013db0dc
zhaocchen <zhaocchen@163.com>
2021-01-27 17:51:18 +0800

    [fix] 98840 大屏编辑--文本框字体样式不生效 [fix] 98843 大屏编辑--指标展示字体样式多个字体显示一样

--- simo-leaderview-vue/src/App.vue
+++ simo-leaderview-vue/src/App.vue
@@ -38,7 +38,9 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss">
+@import 'main.scss';
+
 html,
 body {
   width: 100%;
@@ -69,6 +71,7 @@ body {
 #app {
   width: 100%;
   height: 100%;
+ /* 指标显示器字体，时间器字体 */
 }
 /* 框选区域样式 */
 #bottom {
