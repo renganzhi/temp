0e38058383ad8285e9a02ece5852d6a221fa30df
fanjialong <520544fjl>
2021-02-03 15:08:52 +0800

    功能 #100170视频缩略图替换

--- simo-leaderview-vue/src/views/Edit/Edit.js
+++ simo-leaderview-vue/src/views/Edit/Edit.js
@@ -2931,7 +2931,7 @@ export default {
         $('<img>')
           .addClass('monitp')
           // .attr('src', gbs.host + '/leaderview/border/videoBg.png')
-          .attr('src', gbs.host + '/leaderview/border/videoBg.png')
+          .attr('src', gbs.host + '/leaderview/border/videoBg2.png')
           .css({
             width: '100%',
             height: '100%',
