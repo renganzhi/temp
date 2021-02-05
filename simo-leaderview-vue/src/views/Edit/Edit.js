4c816cb8dbe3d4d85132ca27b73ebde7f6bc036f
fanjialong <520544fjl>
2021-02-05 10:01:40 +0000

    缺陷 #100844大屏编辑--表格背景图无法删除
    (cherry picked from commit 16d9df363716fb6cd8115883cbabbb04bbd571e5)

--- simo-leaderview-vue/src/views/Edit/Edit.js
+++ simo-leaderview-vue/src/views/Edit/Edit.js
@@ -3583,6 +3583,8 @@ export default {
         this.selectedItem.imgSrc = ''
       } else if (chartType === 'video') {
         this.selectedItem.videoSrc = ''
+      } else if (chartType === 'table') {
+        this.selectedItem.tableBack = ''
       }
     },
     // 视频
