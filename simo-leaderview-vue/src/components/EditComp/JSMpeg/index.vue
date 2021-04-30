0cb4d1e118b1434c5b65e25b214f7378fc30ff14
wang_yijia <wang_yijia@uxsino.com>
2021-04-30 13:43:03 +0800

    缺陷 #106890  大屏编辑--实时视频默认状态描述不正确

--- simo-leaderview-vue/src/components/EditComp/JSMpeg/index.vue
+++ simo-leaderview-vue/src/components/EditComp/JSMpeg/index.vue
@@ -9,9 +9,8 @@
     <div class="v-charts-data-empty"
         v-show="!vidoeShow"
         style="width: 100%; height: 100%; text-align: center; font-size: 12px;">
-        <div><i class="icon-n-nodata"
-            style="font-size: 108px;"></i><br>
-          <p>抱歉，没有数据可供展示...</p>
+        <div>
+          <p>请选择视频</p>
         </div>
     </div>
   </div>
