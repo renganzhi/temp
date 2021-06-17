2a255159717f1b02412b05d62555c4a2f971e65c
fanjialong <fan_jialong@uxsino.com>
2021-06-17 13:51:15 +0800

    在线地图图标调整

--- simo-leaderview-vue/src/components/EditComp/BaiDuMap/config.js
+++ simo-leaderview-vue/src/components/EditComp/BaiDuMap/config.js
@@ -1,7 +1,7 @@
 export default {
   'item': {
     'text': '在线地图',
-    'imgClass': 'icon-3dlines',
+    'imgClass': 'icon-dituonline',
     'chartType': 'BaiDuMap',
     'width': 300,
     'height': 300
