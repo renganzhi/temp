26459d49aa6ffc0c44e8d85518e5e27f6c8f051a
wang_yijia <wang_yijia@uxsino.com>
2021-08-13 02:59:47 +0000

    【功能开发】功能ID：116138 功能描述：装饰元件新增“炫酷地球”
    (cherry picked from commit e0937cec5f93dd8df5be7c33d48feac5f4171c4e)

--- simo-leaderview-vue/src/components/EditComp/decorator/index.js
+++ simo-leaderview-vue/src/components/EditComp/decorator/index.js
@@ -1,11 +1,22 @@
 import Vue from 'vue'
 import { gbs } from '@/config/settings'
+import GreenEarth from '@/../src/components/EditComp/CoolEarth/GreenEarth'
+import VioletEarth from '@/../src/components/EditComp/CoolEarth/VioletEarth'
+import BlueEarth from '@/../src/components/EditComp/CoolEarth/BlueEarth'
 
 export default Vue.component('decorator', {
-  template: `<img :style="wrapStyle" 
-        :src="baseUrl + item.imgSrc"
-    ></img>`,
+  template: `
+  <GreenEarth v-if="item.imgSrc === 'GreenEarth'"  :item=earthStyle></GreenEarth>
+  <VioletEarth v-else-if="item.imgSrc === 'VioletEarth'" :item=earthStyle></VioletEarth>
+  <BlueEarth v-else-if="item.imgSrc === 'BlueEarth'" :item=earthStyle></BlueEarth>
+  <img v-else :style="wrapStyle" 
+        :src="baseUrl + item.imgSrc"     
+    ></img>
+
+  
+    `,
   props: ['item'],
+  components: {GreenEarth, VioletEarth, BlueEarth},
   data () {
     return {
       baseUrl: gbs.host
@@ -21,6 +32,21 @@ export default Vue.component('decorator', {
         style = {
           maxWidth: '100%',
           maxHeight: '100%'
+        }
+      }
+      return style
+    },
+    earthStyle () {
+      let style = {
+        width: this.item.width,
+        height: this.item.height,
+        showType: this.item.showType
+      }
+      if (this.item.showType && this.item.showType === '1') {
+        style = {
+          width: this.item.width > this.item.height ? this.item.height : this.item.width,
+          height: this.item.width > this.item.height ? this.item.height : this.item.width,
+          showType: this.item.showType
         }
       }
       return style
