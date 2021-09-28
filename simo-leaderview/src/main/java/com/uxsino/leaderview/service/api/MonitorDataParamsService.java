9eedee76f121d824c8f71e708223eb0122029730
he_xudong <he_xudong@uxsino.com>
2021-09-28 16:08:48 +0800

    【类型】缺陷 #120977【描述】【大屏模板】业务大屏，主机CPU默认统计最近1小时数据，数据量大时X轴的时间显示不全(调整选择名称，增加了年月日时分的选项)

--- simo-leaderview/src/main/java/com/uxsino/leaderview/service/api/MonitorDataParamsService.java
+++ simo-leaderview/src/main/java/com/uxsino/leaderview/service/api/MonitorDataParamsService.java
@@ -1560,8 +1560,9 @@ public class MonitorDataParamsService {
     public JsonModel getDateFormat() {
         JSONArray res = new JSONArray();
         res.add(newResultObj("年-月-日 时:分:秒","yyyy-MM-dd hh:mm:ss"));
-        res.add(newResultObj("年-月-日","yyyy-MM-dd"));
+        res.add(newResultObj("年-月-日 时:分","yyyy-MM-dd hh:mm"));
         res.add(newResultObj("月-日 时:分","MM-dd hh:mm"));
+        res.add(newResultObj("年-月-日","yyyy-MM-dd"));
         res.add(newResultObj("时:分:秒","hh:mm:ss"));
         return new JsonModel(true,res);
     }
