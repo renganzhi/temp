b689795e4d82eb94a922f31021dd6b8c0caff01c
wang_yijia <wang_yijia@uxsino.com>
2023-01-28 14:57:40 +0800

    优化城运
    (cherry picked from commit 3777d7fbdb24c96afae0f8c1afb87bab5791ad28)

--- simo-leaderview-vue/src/components/EditComp/EconomicMap/index.vue
+++ simo-leaderview-vue/src/components/EditComp/EconomicMap/index.vue
@@ -46,7 +46,9 @@ export default {
   },
   methods: {
     initMap () {
-      this.map = new window.BMapGL.Map('cyMap')
+      this.map = new window.BMapGL.Map('cyMap', {
+        restrictCenter: false
+      })
       this.centerPoint = turf.centerOfMass(Region).geometry.coordinates // 定义一个中心点坐标
       this.map.centerAndZoom(new window.BMapGL.Point(this.centerPoint[0], this.centerPoint[1]), 5) // 设定地图的中心点和坐标并将地图显示在地图容器中 104.02959, 30.60570
       this.map.enableScrollWheelZoom(true) // 开启鼠标滚轮缩放
@@ -54,16 +56,16 @@ export default {
         map: this.map
       })
       // this.map.setMapStyleV2({styleJson: styleJson})
-      // this.map.setOptions({
-      //   style: {
-      //     styleJson: styleJson
-      //   },
-      //   styleUrl: 'http://172.16.152.196:8219/baidumap/bmapgl/mapstyle/new_mapStyle.json'
-      // })
-      // this.map.setDisplayOptions({
-      //   poiText: false
-      // })
-      // this.getFQ()
+      this.map.setOptions({
+        style: {
+          styleJson: styleJson
+        },
+        styleUrl: 'http://172.16.152.196:8219/baidumap/bmapgl/mapstyle/new_mapStyle.json'
+      })
+      this.map.setDisplayOptions({
+        poiText: false
+      })
+      this.getFQ()
     },
     getFQ () {
       console.log('re', Region)
