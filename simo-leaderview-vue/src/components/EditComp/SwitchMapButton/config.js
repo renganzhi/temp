b2860efb3c24a285c3cb960f50f170ee887d4540
wang_yijia <wang_yijia@uxsino.com>
2023-02-20 15:46:28 +0800

    修改
    (cherry picked from commit 026ad9900488477b35a032c60ad9daa2bcc192ba)

--- simo-leaderview-vue/src/components/EditComp/SwitchMapButton/config.js
+++ simo-leaderview-vue/src/components/EditComp/SwitchMapButton/config.js
@@ -6,10 +6,32 @@ export default{
     'width': 870,
     'height': 660,
     'size': 'big',
+    animation: true,
     'chartData': {}
   },
   'styles': {
     'base': [
+      {
+        'name': '其他设置',
+        'tag': 'Hint',
+        'childoption': [
+          {
+            'name': '是否播放动画',
+            'key': 'animation',
+            'tag': 'select',
+            'options': [
+              {
+                'name': '开启',
+                'value': true
+              },
+              {
+                'name': '关闭',
+                'value': false
+              }
+            ]
+          }
+        ]
+      }
     ]
   }
 }
